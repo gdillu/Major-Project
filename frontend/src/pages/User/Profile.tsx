b908a2088441cf72f8299e7a10f28a688a11ac8e
gdillu <gdreddy255@gmail.com>
2023-04-15 12:14:24 +0530

    first commit

---
+++ frontend/src/pages/User/Profile.tsx
@@ -0,0 +1,33 @@
+import React, { useContext } from "react";
+import { RouteProps } from "react-router";
+import { AuthContext } from "../../contexts/Auth";
+
+const Profile = (props: RouteProps) => {
+  const authContext = useContext(AuthContext);
+
+  console.log({ authContext });
+
+  return (
+    <div className="profile-wrapper">
+      <div className="left-panel">
+        <div className="person-icon">
+          <i className="bi bi-person-circle"></i>
+        </div>
+        <div className="text-normal username">{authContext.name}</div>
+        <button onClick={authContext.logout} className="button-primary">
+          Logout
+        </button>
+      </div>
+
+      <div className="right-panel">
+        <span className="title-small">Profile</span>
+        <div className="text-profile" >{authContext.id}</div>
+        <div className="text-profile">{authContext.email}</div>
+        <div className="text-profile">{authContext.citizen}</div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
